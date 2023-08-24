2178badcacf36c0b833161694aaec576988dbbbf
MuhammedKB <mohamad.kabbani@ogr.sakarya.edu.tr>
2023-08-24 22:11:40 +0300

    Autofac IoC Container eklendi.

---
+++ BusinessLayer/DependecyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.DependecyResolvers.Autofac
+{
+	public class AutofacBusinessModule : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterType<EmployeeManager>().As<IEmployeeService>().SingleInstance();
+			builder.RegisterType<EFEmployeeDal>().As<IEmployeeDal>().SingleInstance();
+		}
+	}
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using BusinessLayer.Abstract;
 using BusinessLayer.Concrete;
+using BusinessLayer.DependecyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -14,21 +17,25 @@ namespace WebAPI
 			// Add services to the container.
 
 			builder.Services.AddControllers();
-			builder.Services.AddSingleton<IEmployeeService, EmployeeManager>();
-			builder.Services.AddSingleton<IEmployeeDal, EFEmployeeDal>();
+
+			// Set up Autofac
+			var containerBuilder = new ContainerBuilder();
+			containerBuilder.Populate(builder.Services);
+
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
-
+			builder.Host
+				.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+				.ConfigureContainer<ContainerBuilder>(cb => { cb.RegisterModule(new AutofacBusinessModule()); });
 			var app = builder.Build();
-
+			// Use the Autofac container as the service provider
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
 			{
 				app.UseSwagger();
 				app.UseSwaggerUI();
 			}
-
 			app.UseHttpsRedirection();
 
 			app.UseAuthorization();
